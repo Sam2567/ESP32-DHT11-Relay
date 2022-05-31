d13798e1c89185581c5cfc7cd583be2e012ba69c
Sam <qscrev98@gmail.com>
2022-05-31 17:59:16 +0930

    Revert "Added a testing web_server"
    This reverts commit fdf5ff2410aa97bc9d4e2bce2fea28d6c98476a7.

--- main/http.h
+++
@@ -1,4 +0,0 @@
-#include "esp_http_server.h"
-
-inline size_t MIN(size_t a, size_t b);
-inline size_t MAX(size_t a, size_t b);
--- main/mqtt.c
+++ main/mqtt.c
@@ -1,5 +1,49 @@
-#include "mqtt.h"
 
+#include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include <string.h>
+#include <stdint.h>
+#include <stddef.h>
+#include "freertos/event_groups.h"
+#include "freertos/semphr.h"
+#include "freertos/queue.h"
+#include "esp_system.h"
+#include "esp_wifi.h"
+#include "esp_event.h"
+#include "esp_log.h"
+#include "nvs_flash.h"
+#include "lwip/err.h"
+#include "lwip/sys.h"
+#include "lwip/sockets.h"
+#include "lwip/dns.h"
+#include "lwip/netdb.h"
+#include "mqtt_client.h"
+#include "dht11.h"
+#include "cJSON.h"
+#include "esp_smartconfig.h"
+/* The event group allows multiple bits for each event, but we only care about two events:
+ * - we are connected to the AP with an IP
+ * - we failed to connect after the maximum amount of retries */
+
+
+
+#define GPIO_OUTPUT_IO_3     3
+#define GPIO_OUTPUT_IO_4     4
+#define MAXIMUM_RETRY 10
+#define WIFI_FAIL_BIT      BIT1
+#define CONNECTED_BIT      BIT0
+#define ESPTOUCH_DONE_BIT  BIT1
+int retry = 0;
+
+esp_mqtt_client_handle_t client_init;
+
+static const char *TAG = "ESP Relay";
+/* FreeRTOS event group to signal when we are connected & ready to make a request */
+static EventGroupHandle_t s_wifi_event_group;
+
+
+static void smartconfig_task(void * parm);
 
 static void event_handler(void* arg, esp_event_base_t event_base,
                                 int32_t event_id, void* event_data)
@@ -13,9 +57,9 @@ static void event_handler(void* arg, esp_event_base_t event_base,
         } else {
             xEventGroupSetBits(s_wifi_event_group, WIFI_FAIL_BIT);
             ESP_LOGI(TAG,"SC_task start");
-            xTaskCreate(smartconfig_task, "smartconfig_task", 4096, NULL, 6, NULL);
         }
         ESP_LOGI(TAG,"Connect to the WIFI fail");
+        
     } else if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_DISCONNECTED) {
         if (retry < MAXIMUM_RETRY) {
             esp_wifi_connect();
@@ -25,8 +69,8 @@ static void event_handler(void* arg, esp_event_base_t event_base,
         } else {
             xEventGroupSetBits(s_wifi_event_group, WIFI_FAIL_BIT);
             ESP_LOGI(TAG,"Connect to the WIFI fail");
-            xTaskCreate(smartconfig_task, "smartconfig_task", 4096, NULL, 6, NULL);
         }
+        
     } else if (event_base == IP_EVENT && event_id == IP_EVENT_STA_GOT_IP) {
         xEventGroupSetBits(s_wifi_event_group, CONNECTED_BIT);
     } else if (event_base == SC_EVENT && event_id == SC_EVENT_SCAN_DONE) {
@@ -124,14 +168,14 @@ static esp_err_t mqtt_event_handler_cb(esp_mqtt_event_handle_t event)
         case MQTT_EVENT_CONNECTED:
             ESP_LOGI(TAG, "MQTT_EVENT_CONNECTED");
 
-            msg_id = esp_mqtt_client_subscribe(client, MQTT_CONTROL, 0);
+            msg_id = esp_mqtt_client_subscribe(client, "mqtt/bedroom/power_relay/control/1", 0);
             
             ESP_LOGI(TAG, "sent subscribe successful, msg_id=%d", msg_id);
 
             break;
         case MQTT_EVENT_DISCONNECTED:
             ESP_LOGI(TAG, "MQTT_EVENT_DISCONNECTED");
-            msg_id = esp_mqtt_client_subscribe(client, MQTT_CONTROL, 0);
+            msg_id = esp_mqtt_client_subscribe(client, "mqtt/bedroom/power_relay/control/1", 0);
             
             break;
 
@@ -149,9 +193,9 @@ static esp_err_t mqtt_event_handler_cb(esp_mqtt_event_handle_t event)
             int switc = gpio_get_level(GPIO_OUTPUT_IO_4);
             if( switc == 1){
                 gpio_set_level(GPIO_OUTPUT_IO_4, 0);
-                msg_id = esp_mqtt_client_publish(client, MQTT_STATUS, "OFF", 0, 0, false);
+                msg_id = esp_mqtt_client_publish(client, "mqtt/bedroom/power_relay/1", "OFF", 0, 0, false);
             } else {
-                msg_id = esp_mqtt_client_publish(client, MQTT_STATUS, "ON", 0, 0, false);
+                msg_id = esp_mqtt_client_publish(client, "mqtt/bedroom/power_relay/1", "ON", 0, 0, false);
                 gpio_set_level(GPIO_OUTPUT_IO_4, 1);
             }
             break;
