69610f3d519c99305c1865ee0f843ed24fe009ef
Sam <qscrev98@gmail.com>
2022-05-31 21:00:03 +0930

    remove smartconfig

--- main/mqtt.h
+++ main/mqtt.h
@@ -19,7 +19,7 @@
 #include "mqtt_client.h"
 #include "dht11.h"
 #include "cJSON.h"
-#include "esp_smartconfig.h"
+
 /* The event group allows multiple bits for each event, but we only care about two events:
  * - we are connected to the AP with an IP
  * - we failed to connect after the maximum amount of retries */
@@ -42,5 +42,5 @@ static const char *TAG = "ESP Relay";
 static EventGroupHandle_t s_wifi_event_group;
 
 
-static void smartconfig_task(void * parm);
+
 
